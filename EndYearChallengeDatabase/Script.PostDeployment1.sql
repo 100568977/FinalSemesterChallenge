f0bd2e24d67ded6fa876f1f1155c1c7aa5469763
Ikkatos23 <100568977@student.swin.edu.au>
2018-11-28 15:06:50 +1100

    Removed Comment on Script

--- EndYearChallengeDatabase/Script.PostDeployment1.sql
+++ EndYearChallengeDatabase/Script.PostDeployment1.sql
@@ -3,14 +3,14 @@
 BEGIN
 
 DELETE FROM [Match]
---DELETE FROM [Member]
+DELETE FROM [Member]
 
---INSERT INTO [Member]([Surname], [FirstName], [Authorised], [Email]) VALUES
---('Angry', 'Fangry', 1, 'Charles@email.com'),
---('Slow', 'Kyle', 1, 'Kyle@email.com'),
---('Ruthless', 'Austin', 1, 'Austin@email.com'),
---('Philipie', 'Goobers', 1, 'Adrian@email.com'),
---('James', 'Jilly', 1, 'Billy@email.com');
+INSERT INTO [Member]([Surname], [FirstName], [Authorised], [Email]) VALUES
+('Angry', 'Fangry', 1, 'Charles@email.com'),
+('Slow', 'Kyle', 1, 'Kyle@email.com'),
+('Ruthless', 'Austin', 1, 'Austin@email.com'),
+('Philipie', 'Goobers', 1, 'Adrian@email.com'),
+('James', 'Jilly', 1, 'Billy@email.com');
 
 INSERT INTO [Match]([Date], [Venue], [AmountPaid], [MemberId]) VALUES
 ('2019/11/23 09:00:00.000', 'End ofspace', 500, 2),
